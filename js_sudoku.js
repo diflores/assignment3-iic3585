79e39e902ec7a7ea56abca35a47cd62224c235d1
jatrijori <jtjorquera@uc.cl>
2020-04-30 00:26:13 -0400

    finish js sudoku solver for 9x9 board

--- js_sudoku.js
+++ js_sudoku.js
@@ -44,8 +44,34 @@ const isValidNumber = (board, row, col, number) => {
   return (board[row][col] === 0 && !usedIn3x3(board, row - row % 3, col - col % 3, number) && !usedInRow(board, row, number) && !usedInCol(board, col, number));
 };
 
-const solveSudoku = (flattenSudoku) => {
-  return flattenSudoku;
+const isSolved = (board) => {
+  const position = [0, 0]
+  if (!findEmptyCell(board, position)) {
+    return true;
+  }
+  for (let value = 1; value < 10; value++) {
+    const row = position[0];
+    const col = position[1];
+    if (isValidNumber(board, row, col, value)) {
+      board[row][col] = value;
+      if (isSolved(board)) {
+        return true;
+      }
+      board[row][col] = 0;
+    }
+  }
+  return false;
+}
+
+const solveSudoku = (board) => {
+  isSolved(board);
+  flatBoard = Array(81);
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++){
+      flatBoard[9 * row + col] = board[row][col];
+    }
+  }
+  return flatBoard;
 };
 
 const solve = () => {
@@ -54,7 +80,16 @@ const solve = () => {
     const value = parseInt(document.getElementById(`cell-${i}`).value);
     value ? flattenSudoku.push(value) : flattenSudoku.push(0);
   });
-  const resultArray = solveSudoku(flattenSudoku);
+  const matrixSudoku = [];
+  for (let row = 0; row < 9; row++) {
+    matrixSudoku.push(Array(9));
+  }
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++){
+      matrixSudoku[row][col] = flattenSudoku[9 * row + col];
+    }
+  }
+  const resultArray = solveSudoku(matrixSudoku);
   [...Array(81).keys()].map(i => {
     if (flattenSudoku[i] === 0) {
       document.getElementById(`cell-${i}`).classList.add("filled");
